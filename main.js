1eabfed7f22474d2208da60423b0b3bd26d10607
iberidze38 <iberidze38@gmail.com>
2023-07-26 18:00:08 +0400

    scroll to top

---
+++ main.js
@@ -0,0 +1,25 @@
+let mybutton = document.getElementById("btn-back-to-top");
+
+//ღილაკი გაჩნდება როცა სქროლი გაცდება 20px-ს
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+//ღილაკი ასქროლავს 0px-ზე
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
