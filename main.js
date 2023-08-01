5be1614a3293ef723818db1b68fdbab6e98e465e
irakli <iberidze38@gmail.com>
2023-08-01 23:30:09 +0400

    burger menu

--- main.js
+++ main.js
@@ -27,3 +27,21 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+// burger menu
+
+const close = document.getElementById('close');
+const menuBar = document.getElementById('menu');
+const menuBtn = document.getElementById('menuBtn');
+
+function openMenu(){
+    menuBar.classList.add('active');    
+}
+
+function closeMenu(){
+    menuBar.classList.remove('active'); 
+}
+
+close.addEventListener("click", closeMenu);
+menuBtn.addEventListener("click", openMenu);
