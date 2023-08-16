7f397395b8887c91acd1e6fe6c3c1ea256747dba
Machakhleli <ushangikakhidze@gmail.com>
2023-08-16 17:58:46 +0400

    added read more function in main.js and display none in css

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 // -----------Scroll To Top ღილაკი------
 
 let mybutton = document.getElementById("btn-back-to-top");
@@ -8,10 +7,7 @@ window.onscroll = function () {
 };
 
 function scrollFunction() {
-  if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-  ) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
@@ -28,20 +24,36 @@ function backToTop() {
 // ---------------------------------------------------
 // burger menu
 
-const close = document.getElementById('close');
-const menuBar = document.getElementById('menu');
-const menuBtn = document.getElementById('menuBtn');
+const close = document.getElementById("close");
+const menuBar = document.getElementById("menu");
+const menuBtn = document.getElementById("menuBtn");
 
-function openMenu(){
-    menuBar.classList.add('active');    
+function openMenu() {
+  menuBar.classList.add("active");
 }
 
-function closeMenu(){
-    menuBar.classList.remove('active'); 
+function closeMenu() {
+  menuBar.classList.remove("active");
 }
 
 close.addEventListener("click", closeMenu);
 menuBtn.addEventListener("click", openMenu);
 
+//-----------------------------------------------------
+// read more
 
+function readMore() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var rdMore = document.getElementById("readMore");
 
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    rdMore.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    rdMore.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
