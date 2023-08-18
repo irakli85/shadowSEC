541651cdfa7dba600c74e5f50c7cbe09af67cf12
iberidze38 <iberidze38@gmail.com>
2023-08-18 12:58:03 +0400

    fixed team sliders

--- main.js
+++ main.js
@@ -58,38 +58,6 @@ function readMore() {
   }
 }
 
-
-//------------------// swiper for team
-
-// var teamSwiper = new Swiper(".mySwiper2", {
-//   slidesPerView: 3,
-//   loop: true,
-//   autoplay: {
-//       delay: 2500,
-//       disableOnInteraction: false,
-//   },
-//   navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//   },
-//   breakpoints: {
-//       // when window width is >= 320px
-//       320: {
-//           slidesPerView: 1,
-//       },
-//       // when window width is >= 480px
-//       480: {
-//           slidesPerView: 1,
-//       },
-//       // when window width is >= 640px
-//       640: {
-//           slidesPerView: 3,
-//       }
-//   }
-// });
-
-
-//tea
 //read more 
 function myFunction() {
   var dots = document.getElementById("dots");
