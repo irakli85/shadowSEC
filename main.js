aaa3712208707628935c8f9851f75e2756067c27
Machakhleli <ushangikakhidze@gmail.com>
2023-08-23 17:10:03 +0400

    added read more buttons in section six

--- main.js
+++ main.js
@@ -39,26 +39,7 @@ function closeMenu() {
 close.addEventListener("click", closeMenu);
 menuBtn.addEventListener("click", openMenu);
 
-//----------------------------------------------------
-// read more
-
-function readMore() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var rdMore = document.getElementById("readMore");
-
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    rdMore.innerHTML = "Read more";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    rdMore.innerHTML = "Read less";
-    moreText.style.display = "inline";
-  }
-}
-
-//read more 
+//read more
 function myFunction() {
   var dots = document.getElementById("dots");
   var moreText = document.getElementById("more");
@@ -74,7 +55,6 @@ function myFunction() {
     moreText.style.display = "inline";
   }
 }
-
 
 //read more 1
 function myFunction1() {
@@ -127,3 +107,291 @@ function myFunction3() {
   }
 }
 
+//----------------------------------------------------
+// read more for team
+
+//------ gocha gogiberidze
+
+function myFunction4() {
+  var dots4 = document.getElementById("dots4");
+  var moreText4 = document.getElementById("more4");
+  var btnText4 = document.getElementById("myBtn4");
+
+  if (dots4.style.display === "none") {
+    dots4.style.display = "inline";
+    btnText4.innerHTML = "Read more";
+    moreText4.style.display = "none";
+  } else {
+    dots4.style.display = "none";
+    btnText4.innerHTML = "Read less";
+    moreText4.style.display = "inline";
+  }
+}
+
+//--------- david chakvetadze
+function myFunction5() {
+  var dots5 = document.getElementById("dots5");
+  var moreText5 = document.getElementById("more5");
+  var btnText5 = document.getElementById("myBtn5");
+
+  if (dots5.style.display === "none") {
+    dots5.style.display = "inline";
+    btnText5.innerHTML = "Read more";
+    moreText5.style.display = "none";
+  } else {
+    dots5.style.display = "none";
+    btnText5.innerHTML = "Read less";
+    moreText5.style.display = "inline";
+  }
+}
+
+//----gela tabatadze
+function myFunction6() {
+  var dots6 = document.getElementById("dots6");
+  var moreText6 = document.getElementById("more6");
+  var btnText6 = document.getElementById("myBtn6");
+
+  if (dots6.style.display === "none") {
+    dots6.style.display = "inline";
+    btnText6.innerHTML = "Read more";
+    moreText6.style.display = "none";
+  } else {
+    dots6.style.display = "none";
+    btnText6.innerHTML = "Read less";
+    moreText6.style.display = "inline";
+  }
+}
+
+//----zviad eliziani
+function myFunction7() {
+  var dots7 = document.getElementById("dots7");
+  var moreText7 = document.getElementById("more7");
+  var btnText7 = document.getElementById("myBtn7");
+
+  if (dots7.style.display === "none") {
+    dots7.style.display = "inline";
+    btnText7.innerHTML = "Read more";
+    moreText7.style.display = "none";
+  } else {
+    dots7.style.display = "none";
+    btnText7.innerHTML = "Read less";
+    moreText7.style.display = "inline";
+  }
+}
+
+//---- kakha margiani
+
+function myFunction8() {
+  var dots8 = document.getElementById("dots8");
+  var moreText8 = document.getElementById("more8");
+  var btnText8 = document.getElementById("myBtn8");
+
+  if (dots8.style.display === "none") {
+    dots8.style.display = "inline";
+    btnText8.innerHTML = "Read more";
+    moreText8.style.display = "none";
+  } else {
+    dots8.style.display = "none";
+    btnText8.innerHTML = "Read less";
+    moreText8.style.display = "inline";
+  }
+}
+
+//------- levan otarashvili
+
+function myFunction9() {
+  var dots9 = document.getElementById("dots9");
+  var moreText9 = document.getElementById("more9");
+  var btnText9 = document.getElementById("myBtn9");
+
+  if (dots9.style.display === "none") {
+    dots9.style.display = "inline";
+    btnText9.innerHTML = "Read more";
+    moreText9.style.display = "none";
+  } else {
+    dots9.style.display = "none";
+    btnText9.innerHTML = "Read less";
+    moreText9.style.display = "inline";
+  }
+}
+
+//-------- miranda gorgiladze
+
+function myFunction10() {
+  var dots10 = document.getElementById("dots10");
+  var moreText10 = document.getElementById("more10");
+  var btnText10 = document.getElementById("myBtn10");
+
+  if (dots10.style.display === "none") {
+    dots10.style.display = "inline";
+    btnText10.innerHTML = "Read more";
+    moreText10.style.display = "none";
+  } else {
+    dots10.style.display = "none";
+    btnText10.innerHTML = "Read less";
+    moreText10.style.display = "inline";
+  }
+}
+
+//------ rezo bakuridze
+
+function myFunction11() {
+  var dots11 = document.getElementById("dots11");
+  var moreText11 = document.getElementById("more11");
+  var btnText11 = document.getElementById("myBtn11");
+
+  if (dots11.style.display === "none") {
+    dots11.style.display = "inline";
+    btnText11.innerHTML = "Read more";
+    moreText11.style.display = "none";
+  } else {
+    dots11.style.display = "none";
+    btnText11.innerHTML = "Read less";
+    moreText11.style.display = "inline";
+  }
+}
+
+//-------nodar natidze
+
+function myFunction12() {
+  var dots12 = document.getElementById("dots12");
+  var moreText12 = document.getElementById("more12");
+  var btnText12 = document.getElementById("myBtn12");
+
+  if (dots12.style.display === "none") {
+    dots12.style.display = "inline";
+    btnText12.innerHTML = "Read more";
+    moreText12.style.display = "none";
+  } else {
+    dots12.style.display = "none";
+    btnText12.innerHTML = "Read less";
+    moreText12.style.display = "inline";
+  }
+}
+
+//---- international team
+//---- ralf braga
+
+function myFunction13() {
+  var dots13 = document.getElementById("dots13");
+  var moreText13 = document.getElementById("more13");
+  var btnText13 = document.getElementById("myBtn13");
+
+  if (dots13.style.display === "none") {
+    dots13.style.display = "inline";
+    btnText13.innerHTML = "Read more";
+    moreText13.style.display = "none";
+  } else {
+    dots13.style.display = "none";
+    btnText13.innerHTML = "Read less";
+    moreText13.style.display = "inline";
+  }
+}
+
+//---------david brown
+
+function myFunction14() {
+  var dots14 = document.getElementById("dots14");
+  var moreText14 = document.getElementById("more14");
+  var btnText14 = document.getElementById("myBtn14");
+
+  if (dots14.style.display === "none") {
+    dots14.style.display = "inline";
+    btnText14.innerHTML = "Read more";
+    moreText14.style.display = "none";
+  } else {
+    dots14.style.display = "none";
+    btnText14.innerHTML = "Read less";
+    moreText14.style.display = "inline";
+  }
+}
+
+//------ maria dimitrova
+
+function myFunction15() {
+  var dots15 = document.getElementById("dots15");
+  var moreText15 = document.getElementById("more15");
+  var btnText15 = document.getElementById("myBtn15");
+
+  if (dots15.style.display === "none") {
+    dots15.style.display = "inline";
+    btnText15.innerHTML = "Read more";
+    moreText15.style.display = "none";
+  } else {
+    dots15.style.display = "none";
+    btnText15.innerHTML = "Read less";
+    moreText15.style.display = "inline";
+  }
+}
+
+//----- edward valenti
+
+function myFunction16() {
+  var dots16 = document.getElementById("dots16");
+  var moreText16 = document.getElementById("more16");
+  var btnText16 = document.getElementById("myBtn16");
+
+  if (dots16.style.display === "none") {
+    dots16.style.display = "inline";
+    btnText16.innerHTML = "Read more";
+    moreText16.style.display = "none";
+  } else {
+    dots16.style.display = "none";
+    btnText16.innerHTML = "Read less";
+    moreText16.style.display = "inline";
+  }
+}
+
+//------- ken de boer
+
+function myFunction17() {
+  var dots17 = document.getElementById("dots17");
+  var moreText17 = document.getElementById("more17");
+  var btnText17 = document.getElementById("myBtn17");
+
+  if (dots17.style.display === "none") {
+    dots17.style.display = "inline";
+    btnText17.innerHTML = "Read more";
+    moreText17.style.display = "none";
+  } else {
+    dots17.style.display = "none";
+    btnText17.innerHTML = "Read less";
+    moreText17.style.display = "inline";
+  }
+}
+
+//----dr patrick mcilwee
+
+function myFunction18() {
+  var dots18 = document.getElementById("dots18");
+  var moreText18 = document.getElementById("more18");
+  var btnText18 = document.getElementById("myBtn18");
+
+  if (dots18.style.display === "none") {
+    dots18.style.display = "inline";
+    btnText18.innerHTML = "Read more";
+    moreText18.style.display = "none";
+  } else {
+    dots18.style.display = "none";
+    btnText18.innerHTML = "Read less";
+    moreText18.style.display = "inline";
+  }
+}
+
+// ----- melaine valenti
+
+function myFunction19() {
+  var dots19 = document.getElementById("dots19");
+  var moreText19 = document.getElementById("more19");
+  var btnText19 = document.getElementById("myBtn19");
+
+  if (dots19.style.display === "none") {
+    dots19.style.display = "inline";
+    btnText19.innerHTML = "Read more";
+    moreText19.style.display = "none";
+  } else {
+    dots19.style.display = "none";
+    btnText19.innerHTML = "Read less";
+    moreText19.style.display = "inline";
+  }
+}
